b1b1e7f03a2689edd184af780d29bb8d2aeffe27
dornor <suchaya_a@cmu.ac.th>
2025-02-10 16:04:16 +0700

    Initialize Project FE/BE

---
+++ leave-system/src/main/java/com/dornor/leave_system/LeaveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dornor.leave_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveSystemApplication.class, args);
+	}
+
+}
---
+++ leave-system/src/test/java/com/dornor/leave_system/LeaveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dornor.leave_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
