6ab7a910dfe3daff223011a96dac99153b0cbc42
dornor <suchaya_a@cmu.ac.th>
2025-02-12 18:35:24 +0700

    Gen components

---
+++ leave-system-app/src/app/components/leave-form/leave-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './leave-form.component.html',
+  styleUrl: './leave-form.component.css'
+})
+export class LeaveFormComponent {
+
+}
